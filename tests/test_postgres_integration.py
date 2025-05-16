041070811886284949ac6e46a5cfc253bf58e058
Giovanni <giovanni.nurcis@outlook.it>
2025-05-16 12:38:05 +0200

    Test con database reale

---
+++ tests/test_postgres_integration.py
@@ -0,0 +1,35 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import pytest
+from app import create_app
+from extensions import db
+from models import ThoughtModel
+
+@pytest.fixture
+def client():
+    config = {
+        "TESTING": True,
+        "SQLALCHEMY_DATABASE_URI": os.getenv("DATABASE_URL"),
+        "SQLALCHEMY_TRACK_MODIFICATIONS": False
+    }
+    app = create_app(config)
+
+    with app.test_client() as client:
+        with app.app_context():
+            db.create_all()
+        yield client
+        db.session.remove()
+        db.drop_all()
+
+def test_db_integration_postgres(app_context):
+    thought = ThoughtModel(username="postgres_user", text="test_db")
+    db.session.add(thought)
+    db.session.commit()
+    
+    result = ThoughtModel.query.filter_by(username="postgres_user").first()
+    
+    assert result is not None
+    assert result.text == "test_db"
